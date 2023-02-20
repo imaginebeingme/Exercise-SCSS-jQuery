60b2207cdf6b40f86294cdbd378a8cc71a49b0d1
jahdai <124618019+imaginebeingme@users.noreply.github.com>
2023-02-20 07:55:53 -0500

    Add files via upload

---
+++ Torres_exSCSS/jQuery/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+   $("#close").click(function(){
+        $('.modal-container').css('transform','scale(0)');
+    });
+  
+   $("#open").click(function(){
+        $('.modal-container').css('transform','scale(1)');
+    });
